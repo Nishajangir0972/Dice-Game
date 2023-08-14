963c93d505c8563b80d3d1e0208c0988a467f660
Nishajangir0972 <nishajangir9302@gmail.com>
2023-08-14 12:22:57 -0700

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const playButton=document.querySelector("button" )
+const screen1 = document.querySelector("#screen1");
+const screen2 = document.querySelector("#screen2");
+
+
+playButton.onclick = () => {
+    screen1.style.display = "none";
+    screen2.style.display = "display";
+  };
+
+  btns.onclick = () =>{
+
+  }
